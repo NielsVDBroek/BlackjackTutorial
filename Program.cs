4dcf55faff2c9a968c890b49159a6f05b52cd589
NielsVDBroek <Niels.BroekVanDen@gmail.com>
2024-03-12 11:28:17 +0100

    Deck is now fully functional.

--- Program.cs
+++ Program.cs
@@ -12,36 +12,46 @@ namespace BlackjackTutorial
         Spades
     }
 
-    public enum Rank
+    public class Rank
     {
-        Ace = 1,
-        Two = 2,
-        Three = 3,
-        Four = 4,
-        Five = 5,
-        Six = 6,
-        Seven = 7,
-        Eight = 8,
-        Nine = 9,
-        Ten = 10,
-        Jack = 10,
-        Queen = 10,
-        King = 10
+        public string Name { get; }
+        public int Value { get; }
+
+        public Rank(string name, int value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public static readonly Rank Ace = new Rank("Ace", 1);
+        public static readonly Rank Two = new Rank("Two", 2);
+        public static readonly Rank Three = new Rank("Three", 3);
+        public static readonly Rank Four = new Rank("Four", 4);
+        public static readonly Rank Five = new Rank("Five", 5);
+        public static readonly Rank Six = new Rank("Six", 6);
+        public static readonly Rank Seven = new Rank("Seven", 7);
+        public static readonly Rank Eight = new Rank("Eight", 8);
+        public static readonly Rank Nine = new Rank("Nine", 9);
+        public static readonly Rank Ten = new Rank("Ten", 10);
+        public static readonly Rank Jack = new Rank("Jack", 10);
+        public static readonly Rank Queen = new Rank("Queen", 10);
+        public static readonly Rank King = new Rank("King", 10);
+
+        public static IEnumerable<Rank> AllRanks => new[] { Ace, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King };
     }
 
     public class Card
     {
         public Suit Suit { get; private set; }
         public Rank Rank { get; private set; }
-        public int Value { get; private set; }
 
         public Card(Suit suit, Rank rank)
         {
             Suit = suit;
             Rank = rank;
-            Value = (int)rank;
-
         }
+
+        public int Value => Rank.Value;
     }
 
     public class Deck
@@ -57,9 +67,9 @@ namespace BlackjackTutorial
 
         private void CreateDeck()
         {
-            foreach (Suit suit in System.Enum.GetValues(typeof(Suit)))
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
             {
-                foreach (Rank rank in System.Enum.GetValues(typeof(Rank)))
+                foreach (Rank rank in Rank.AllRanks)
                 {
                     cards.Add(new Card(suit, rank));
                 }
@@ -81,12 +91,10 @@ namespace BlackjackTutorial
 
         public void DrawCard()
         {
-            Card CurrentCard;
-
-            CurrentCard = cards[0];
+            Card CurrentCard = cards[0];
 
             Console.WriteLine("You have drawn a card:");
-            Console.WriteLine($"{CurrentCard.Rank} of {CurrentCard.Suit} (Value: {CurrentCard.Value})");
+            Console.WriteLine($"{CurrentCard.Rank.Name} of {CurrentCard.Suit} (Value: {CurrentCard.Value})");
 
             cards.RemoveAt(0);
         }
@@ -109,8 +117,11 @@ namespace BlackjackTutorial
 
             foreach (var card in cards)
             {
-                Console.WriteLine($"{card.Rank} of {card.Suit} (Value: {card.Value})");
+                Console.WriteLine($"{card.Rank.Name} of {card.Suit} (Value: {card.Value})");
+
             }
+
+            Console.WriteLine("Start of game");
 
             while (true)
             {
@@ -120,6 +131,9 @@ namespace BlackjackTutorial
                 {
                     deck.DrawCard();
                 } else if (input == "stand") {
+                    Console.WriteLine("Player stands");
+                } else if(input == "break")
+                {
                     break;
                 }
             }
